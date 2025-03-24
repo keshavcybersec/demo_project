53ec7315fadf533b5bc466a95d5df7bd9492d421
Keshav Goyal <157672219+keshavcybersec@users.noreply.github.com>
2025-03-24 09:48:01 +0530

    Create multi.py

---
+++ multi.py
@@ -0,0 +1,12 @@
+def multi_numbers(num1, num2):
+    return num1 * num2
+
+# Take input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Perform Multiplication
+multi = multi_numbers(num1, num2)
+
+# Display the result
+print(f"The Multiplication of {num1} and {num2} is: {multi}")
